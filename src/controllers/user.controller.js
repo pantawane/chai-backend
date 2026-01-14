9b317bbc5e0dabf6ac4518b7b386719816e20e4f
Student ApnaCollege <pantawanevansh75@gmail.com>
2026-01-14 12:39:38 +0530

    errors/ debug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -107,10 +107,15 @@ const loginUser = asyncHandler(async (req, res) => {
     //send cookie
 
     const {email, username, password} = req.body
+    console.log(email);
 
-    if(!username || !email ) {
+    if(!username && !email ) {
         throw new ApiError(400, "username or email is required")
     }
+
+    //Here is an alternative of above code based on logic discussion
+    // if (!(username || email)) {
+    //   throw new ApiError(400, "username or email is required") }
 
     const user = await User.findOne({
         $or: [{username}, {email}]
